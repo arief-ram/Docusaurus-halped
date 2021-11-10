e822bfc4ecdd3078b1b654227c1b2bcaaf79924c
uranghishna <hishna.di@gmail.com>
2021-11-10 16:07:26 +0700

    tambah gambar

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/hello">
+            to="/docs/intro">
             Masuk
           </Link>
         </div>
